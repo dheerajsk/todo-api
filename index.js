611a62ae8eeb5eba4f8530e3c18955667a9ba7f8
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-07-09 17:01:42 +0530

    CORS

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const controller = require("./src/todoController");
+const cors = require("cors");
 
 const server = express();
-server.listen(4100);
-
 server.use(bodyParser.json());
+server.use(cors());
 server.post('/api/task',controller.addTask);
 server.get('/api/task',controller.getAllTasks);
 
@@ -15,3 +15,5 @@ server.get("/",(req, res)=>{
 });
 
 console.log("Server is listening on 4100");
+
+server.listen(4100);
--- src/todoController.js
+++ src/todoController.js
@@ -3,8 +3,8 @@ const tasks=[];
 
 exports.addTask = (req, res)=>{
     const newTask = req.body;
-    tasks.push({id: newTask.id, title: newTask.title, dateAdded: new Date()});
-    return res.status(200).send("Task added");
+    tasks.push(newTask);
+    return res.status(200).send(newTask);
 }
 
 exports.getAllTasks = (req, res)=>{
